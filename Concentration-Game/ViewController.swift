47ed7219220f3ae976e27cf2b3f95d3ed7632d41
user165579 <user165579@92-63-141-247.rdns.melbourne.co.uk>
2020-05-05 15:28:42 +0100

    fix cards layout

--- Concentration-Game/ViewController.swift
+++ Concentration-Game/ViewController.swift
@@ -7,23 +7,22 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate {
-   
-    
-  
+//UICollectionViewDelegateFlowLayout
+class ViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate
+{
     @IBOutlet weak var game_CV_cardscollection: UICollectionView!
     var cards : [Card] = [Card]()
     var model = CardModel()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureCardsLayout()
         game_CV_cardscollection.delegate = self
         game_CV_cardscollection.dataSource = self
         cards = model.createCards()
         for card in cards {
             print(card.cardName)
         }
+
     }
 
     // MARK: - UICollectionView Protocol Methods
@@ -42,5 +41,24 @@ class ViewController: UIViewController,UICollectionViewDataSource, UICollectionV
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     
     }
+
+    func configureCardsLayout() {
+        let width = (view.frame.size.width - 40) / 4
+        let layout = game_CV_cardscollection.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width)
+    }
+
+//        override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//            super.viewWillTransition(to: size, with: coordinator)
+//            if UIDevice.current.orientation.isLandscape {
+//                print("Landscape")
+//                let width = (view.frame.size.width - 20) / 3
+//                let layout = game_CV_cardscollection.collectionViewLayout as! UICollectionViewFlowLayout
+//                layout.itemSize = CGSize(width: width, height: width)
+//
+//            } else {
+//                print("Portrait")
+//                        }
+//        }
 }
 
