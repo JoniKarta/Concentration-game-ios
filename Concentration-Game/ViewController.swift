c6bce2bc8e7bcc616af058e91668489cd16ec2af
user165579 <user165579@92-63-141-247.rdns.melbourne.co.uk>
2020-05-05 08:46:54 +0100

    init

---
+++ Concentration-Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration-Game
+//
+//  Created by user165579 on 5/5/20.
+//  Copyright © 2020 user165579. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Concentration-GameTests/Concentration_GameTests.swift
@@ -0,0 +1,34 @@
+//
+//  Concentration_GameTests.swift
+//  Concentration-GameTests
+//
+//  Created by user165579 on 5/5/20.
+//  Copyright © 2020 user165579. All rights reserved.
+//
+
+import XCTest
+@testable import Concentration_Game
+
+class Concentration_GameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
