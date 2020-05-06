e299d7ed514b815c8b9ae10e2644851b92e4f6be
user165579 <user165579@92-63-141-247.rdns.melbourne.co.uk>
2020-05-06 09:22:13 +0100

    added moves and game logic

--- Concentration-Game/CardCollectionViewCell.swift
+++ Concentration-Game/CardCollectionViewCell.swift
@@ -12,4 +12,40 @@ class CardCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var game_IMG_backcard: UIImageView!
     @IBOutlet weak var game_IMG_frontcard: UIImageView!
+    
+    var card : Card?
+    
+    func setCard(_ card: Card){
+        self.card = card
+        
+        self.game_IMG_frontcard.image = UIImage(named: card.cardName)
+    }
+    
+    func flip() {
+        UIView.transition(from: self.game_IMG_backcard,
+                          to: self.game_IMG_frontcard,
+                          duration: 0.3,
+                          options: [.transitionFlipFromLeft,.showHideTransitionViews],
+                          completion: nil)
+    }
+    
+    func flipBack() {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.0){
+            UIView.transition(from: self.game_IMG_frontcard ,
+                              to: self.game_IMG_backcard,
+                duration: 0.3,
+                options: [.transitionFlipFromRight,.showHideTransitionViews],
+                completion: nil)
+        }
+    }
+    
+    func remove() {
+        self.game_IMG_backcard.alpha = 0
+        UIView.animate(withDuration: 0.3,
+                       delay: 0.5,
+                       options: .curveLinear,
+                       animations: {
+                        self.game_IMG_frontcard.alpha = 0
+        }, completion: nil)
+    }
 }
--- Concentration-Game/ViewController.swift
+++ Concentration-Game/ViewController.swift
@@ -7,44 +7,64 @@
 //
 
 import UIKit
-//UICollectionViewDelegateFlowLayout
+
+
 class ViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate
 {
-    @IBOutlet weak var game_CV_cardscollection: UICollectionView!
-    var cards : [Card] = [Card]()
-    var model = CardModel()
+    @IBOutlet weak var game_LBL_moves: UILabel!
+    @IBOutlet weak var game_COLVIEW_cardsCollection: UICollectionView!
+    private var cards : [Card] = [Card]()
+    private var model = CardModel()
+    private var countMoves : Int = 0
+    private var firstCardFlipped : IndexPath?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCardsLayout()
-        game_CV_cardscollection.delegate = self
-        game_CV_cardscollection.dataSource = self
+        game_COLVIEW_cardsCollection.delegate = self
+        game_COLVIEW_cardsCollection.dataSource = self
         cards = model.createCards()
-        for card in cards {
-            print(card.cardName)
-        }
-
     }
-
     // MARK: - UICollectionView Protocol Methods
+   
      func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
            return cards.count
        }
     
-    func collectionView(_ collectionView:UICollectionView,
-                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: "CardView", for: indexPath)
-        
+    func collectionView(_ collectionView:UICollectionView,cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardView", for: indexPath) as! CardCollectionViewCell
+        let card = cards[indexPath.row]
+        cell.setCard(card)
         return cell
     }
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! CardCollectionViewCell
+        cell.flip()
+        let selectedCard = cards[indexPath.row]
+        
+        if selectedCard.isFlipped == false {
+            // Flip the card
+            cell.flip()
+            
+            // Changed the state of the card
+            selectedCard.isFlipped = true
+            
+            if firstCardFlipped == nil{ // Which means it's the first card the player flip
+                firstCardFlipped = indexPath
+            }else{
+                // it's the second card
+                hasMatch(firstCardIndex: firstCardFlipped!, secondCardIndex: indexPath)
+            }
+        }else {
+            cell.flipBack()
+        }
     }
-
+    
+    // MARK: - Configuaration of main layout
     func configureCardsLayout() {
         let width = (view.frame.size.width - 40) / 4
-        let layout = game_CV_cardscollection.collectionViewLayout as! UICollectionViewFlowLayout
+        let layout = game_COLVIEW_cardsCollection.collectionViewLayout as! UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: width, height: width)
     }
 
@@ -60,5 +80,51 @@ class ViewController: UIViewController,UICollectionViewDataSource, UICollectionV
 //                print("Portrait")
 //                        }
 //        }
+    // MARK: - Game logic
+    
+    func hasMatch(firstCardIndex : IndexPath, secondCardIndex : IndexPath) {
+        // Update number of moves in the game
+        updateMove()
+        
+        // Gets cards view
+        let cardOneCellView = game_COLVIEW_cardsCollection.cellForItem(at: firstCardIndex) as? CardCollectionViewCell
+        let cardTwoCellView = game_COLVIEW_cardsCollection.cellForItem(at: secondCardIndex) as? CardCollectionViewCell
+
+        // Get the cards entity
+        let cardOne : Card = self.cards[firstCardIndex.row]
+        let cardTwo : Card = self.cards[secondCardIndex.row]
+        
+        if cardOne.cardName == cardTwo.cardName {
+            
+            // Change the state of the cards
+            cardOne.isMatched = true
+            cardTwo.isMatched = true
+            
+            // Remove the cards from the view
+            cardOneCellView?.remove()
+            cardTwoCellView?.remove()
+            
+        }else{
+        
+            // Change the entity's state
+            cardOne.isFlipped = false
+            cardTwo.isFlipped = false
+            
+            // Flip the cards back
+            cardOneCellView?.flipBack()
+            cardTwoCellView?.flipBack()
+        }
+        // Track if the first cards has flipped
+        self.firstCardFlipped = nil
+        
+    }
+    
+    func updateMove() {
+        self.countMoves = Int(game_LBL_moves.text!)!
+        self.countMoves +=  1
+        self.game_LBL_moves.text = String(countMoves)
+    }
+    
+    
 }
 
